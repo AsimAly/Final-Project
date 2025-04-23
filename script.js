6afc216eaf759d6e9c52211b3e8758603916dbc7
AsimAly <gbiansdevelopers@gmail.com>
2025-04-24 00:18:02 +0500

    Add

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const bar = document.getElementById('fa-solid')
-const ulItem = document.querySelector(".nav-items")
+const ulItem = document.querySelector(".menu")
 
 function clickBar(){
 
     // console.log(unItem.style.left)
 
     if(ultem.style.left==='0px'){
-        unItem.style.left = '-970px'
+        unItem.style.left = '-970px';
 
     }else{
-        ultem.style.left = '0px'
+        ultem.style.left = '0px';
     }
 }
 
-bar.addEventListener("click", clickBar)
+bar.addEventListener("click", clickBar);
