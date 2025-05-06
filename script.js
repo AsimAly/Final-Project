f14ed91361152205d6ea972b68781b92959bb348
AsimAly <gbiansdevelopers@gmail.com>
2025-05-07 01:22:00 +0500

    Updating css

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ const bar = document.querySelector('#fa-solid')
 const ulItem = document.querySelector("#menu-s")
 
 function clickBar(){
-
-    // console.log(unItem.style.left)
-
     if(ulItem.style.left==='0px'){
         ulItem.style.left = '-1026px';
 
